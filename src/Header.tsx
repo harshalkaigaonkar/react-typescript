2461e4e80c41685f707bd713f4a7cffb7ca6e1a3
harshalkaigaonkar <harshalkaigaonkar31@gmail.com>
2021-11-21 01:48:35 +0530

    feat: Hello world App

---
+++ src/Header.tsx
@@ -0,0 +1,18 @@
+export interface Props {
+    title: string
+    color?: string
+}
+
+const Header = (props: Props) => {
+    return (
+        <div>
+            <header>
+                <h1 style={{color: props.color ? props.color: 'blue' }}>
+                    {props.title}
+                </h1>
+            </header>
+        </div>
+    )
+}
+
+export default Header
